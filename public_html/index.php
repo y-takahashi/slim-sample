2e618990cb614c9f0366a3248276c2ccd41084db
y-takahashi <yohei_takahashi@voyagegroup.com>
2013-11-20 17:09:50 +0900

    パス変更

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Reboost\Core\Slim\Slim();
 
-require __DIR__ . '/../app/Reboost/Client/Common/Routes.php';
+require __DIR__ . '/../src/Reboost/Client/Common/Routes.php';
 
 $app->run();
