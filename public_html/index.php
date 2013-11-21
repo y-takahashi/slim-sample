32295ddbb9fac77e68d090cb0b15daf5abccbb4b
y-takahashi <yohei_takahashi@voyagegroup.com>
2013-11-21 10:13:16 +0900

    参考URLを追加

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Slim初期化設定
+// @see http://docs.slimframework.com/#Configuration-Overview
 $app = new \Reboost\Core\Slim\Slim([
     "view" => new \Slim\Views\Twig(),
     "debug" => true,
@@ -9,6 +10,7 @@ $app = new \Reboost\Core\Slim\Slim([
 ]);
 
 // view設定
+// @see https://github.com/codeguy/Slim-Views
 $view = $app->view();
 $view->parserOptions = [
     "debug" => true,
