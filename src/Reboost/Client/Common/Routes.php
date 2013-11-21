a4866f850765a04f0e40cbae3da7ad54989d320f
y-takahashi <yohei_takahashi@voyagegroup.com>
2013-11-21 10:01:46 +0900

    Slim/ViewsのurlFor利用サンプル

--- src/Reboost/Client/Common/Routes.php
+++ src/Reboost/Client/Common/Routes.php
@@ -2,4 +2,4 @@
 
 use Reboost\Client\Controller as Controller;
 
-$app->get('/hello/:name', [Controller\TestController::class, 'index']);
+$app->get('/hello/:name', [Controller\TestController::class, 'index'])->name('hello');
